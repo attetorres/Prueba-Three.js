34184b2859a216c58ff1b2d25b5ad85a6f093a36
attetorres <atteneritorresgonzalez@gmail.com>
2026-03-04 12:17:13 +0000

    feat: cube created

---
+++ main.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+    color: 0xF3E500
+})
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube);
+
+renderer.render(scene, camera);
